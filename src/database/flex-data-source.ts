cf18dd9c2d2332526e8c037f5dffc8e4f5e154a6
Achyut Kumar M <achyutm@gaussiansolutions.com>
2023-06-17 23:36:01 +0530

    fix lint errors

--- src/database/flex-data-source.ts
+++ src/database/flex-data-source.ts
@@ -6,8 +6,6 @@ import UniqueKeyDbException, { ForeignKeyDbException } from '../exceptions/db/da
 class FlexDataSource {
     private pool: Pool = new Pool;
 
-    constructor() {
-    }
 
     public initializaDatabase() {
         console.info("Initializing Flex database !");
@@ -20,7 +18,7 @@ class FlexDataSource {
             port: environment.database.server_port
         });
 
-        this.pool.on('error', function (err: Error, _client: any) {
+        this.pool.on('error', function (err: Error) {
             console.log(`Flex : Idle-Client Error:\n${err.message}\n${err.stack}`);
         }).on('connect', () => {
             console.log("Flex Database initialized successfully !");
