b5bf429a3378fc6af74b948d6845432097d77935
Naresh Kumar D <susrisha@gmail.com>
2023-07-31 15:32:31 +0530

    partially complete with message being sent to service bus
    Service bus message sent.

--- src/controller/gtfs-flex-controller.ts
+++ src/controller/gtfs-flex-controller.ts
@@ -10,17 +10,24 @@ import { FlexVersions } from "../database/entity/flex-version-entity";
 import { validate, ValidationError } from "class-validator";
 import { Version, Versions } from "../model/versions-dto";
 import { environment } from "../environment/environment";
-import multer from "multer";
+import multer, { memoryStorage } from "multer";
 import { GtfsFlexDTO } from "../model/gtfs-flex-dto";
 import { GtfsFlexUploadMeta } from "../model/gtfs-flex-upload-meta";
+import storageService from "../service/storage-service";
+import path from "path";
+import { Readable } from "stream";
+import eventBusService from "../service/event-bus-service";
 
 const upload = multer({
     dest: 'uploads/',
-    // fileFilter: (req, file, cb) => {
-    //     cb(null, false);
-    //     cb(null, true);
-    //     cb(new Error(`I don't have a clue!`));
-    // },
+    storage: memoryStorage(),
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        if(ext != '.zip') {
+            cb(new Error('Invalid file type uploaded'));
+        }
+        cb(null,true);
+    },
 });
 
 class GtfsFlexController implements IController {
@@ -105,6 +112,16 @@ class GtfsFlexController implements IController {
             const result = await validate(gtfsdto);
             console.log('result', result);
             console.log(gtfsdto.collection_date);
+            const uid = storageService.generateRandomUUID();
+            const folderPath = storageService.getFolderPath(gtfsdto.tdei_org_id,uid);
+            // storageService.uploadFile()
+            const uploadedFile = request.file;
+            uploadedFile?.originalname
+            const uploadPath = path.join(folderPath,uploadedFile!.originalname)
+            console.log(uploadPath);
+            // console.log(uploadedFile!.buffer);
+            // console.log(uploadedFile);
+            await storageService.uploadFile(uploadPath,'application/zip',Readable.from(uploadedFile!.buffer))
             // console.log(meta);
             if (!request.body) {
                 response.status(400).send('Input validation failed with below reasons : empty body passed');
@@ -112,38 +129,46 @@ class GtfsFlexController implements IController {
             }
            
 
-            let flex = FlexVersions.from(request.body);
+            let flex = FlexVersions.from(meta);
+            flex.tdei_record_id = uid;
+            flex.file_upload_path = uploadPath;
+            flex.uploaded_by = gtfsdto.collected_by;//TODO: Get the user.
+            // const returnInfo = await gtfsFlexService.createGtfsFlex(flex); 
+            console.log(flex);
+            
+            eventBusService.publishUpload(gtfsdto,uid,uploadPath)
+            // Also send the information to the queueu
+            
+            return response.status(200).send(gtfsdto);
 
-            return response.status(200).send('Happy')
-
-            return validate(flex).then(async errors => {
-                // errors is an array of validation errors
-                if (errors.length > 0) {
-                    console.error('Upload flex file metadata information failed validation. errors: ', errors);
-                    const message = errors.map((error: ValidationError) => Object.values(<any>error.constraints)).join(', ');
-                    response.status(400).send('Input validation failed with below reasons : \n' + message);
-                    next(new HttpException(400, 'Input validation failed with below reasons : \n' + message));
-                } else {
-                    return await gtfsFlexService.createGtfsFlex(flex)
-                        .then(newFlex => {
-                            return Promise.resolve(response.status(200).send(newFlex));
-                        })
-                        .catch((error: any) => {
-                            if (error instanceof DuplicateException) {
-                                response.status(error.status).send(error.message)
-                                next(new HttpException(error.status, error.message));
-                            }
-                            else {
-                                response.status(500).send('Error saving the flex version')
-                                next(new HttpException(500, 'Error saving the flex version'));
-                            }
-                        });
-                }
-            });
+            // return validate(flex).then(async errors => {
+            //     // errors is an array of validation errors
+            //     if (errors.length > 0) {
+            //         console.error('Upload flex file metadata information failed validation. errors: ', errors);
+            //         const message = errors.map((error: ValidationError) => Object.values(<any>error.constraints)).join(', ');
+            //         response.status(400).send('Input validation failed with below reasons : \n' + message);
+            //         next(new HttpException(400, 'Input validation failed with below reasons : \n' + message));
+            //     } else {
+            //         return await gtfsFlexService.createGtfsFlex(flex)
+            //             .then(newFlex => {
+            //                 return Promise.resolve(response.status(200).send(newFlex));
+            //             })
+            //             .catch((error: any) => {
+            //                 if (error instanceof DuplicateException) {
+            //                     response.status(error.status).send(error.message)
+            //                     next(new HttpException(error.status, error.message));
+            //                 }
+            //                 else {
+            //                     response.status(500).send('Error saving the flex version')
+            //                     next(new HttpException(500, 'Error saving the flex version'));
+            //                 }
+            //             });
+            //     }
+            // });
         } catch (error) {
             console.error('Error saving the flex version', error);
             response.status(500).send('Error saving the flex version')
-            next(new HttpException(500, "Error saving the flex version"));
+            // next(new HttpException(500, "Error saving the flex version"));
         }
     }
 }
