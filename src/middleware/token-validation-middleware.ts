41ee8ae16e98201f69b5e0ba99d2e4c9905c71d0
Naresh Kumar D <susrisha@gmail.com>
2023-08-01 11:04:29 +0530

    Added permission request
    Also checks for permission requests

--- src/middleware/token-validation-middleware.ts
+++ src/middleware/token-validation-middleware.ts
@@ -3,29 +3,69 @@
  */
 
 import { Request, Response, NextFunction } from 'express';
-import jwt  from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+import { Core } from 'nodets-ms-core';
+import { environment } from "../environment/environment";
+import { PermissionRequest } from 'nodets-ms-core/lib/core/auth/model/permission_request';
 
-  
+/**
+ * Validates the token and sends the user_id in the req.body
+ * the user id is available as `req.user_id`
+ * @param req - Initial request
+ * @param res  - Supposed response (to be filled by others)
+ * @param next - Next function
+ */
+export async function tokenValidator(req: Request, res: Response, next: NextFunction) {
 
-export function tokenValidator(req: Request, res: Response, next: NextFunction): void {
+    const approvedRoles = ["tdei_admin", "poc", "flex_data_generator"]; // Change it based on service
+    
     // Get the authorization key
     const bearerHeader = req.headers.authorization;
-    if(bearerHeader === ''|| bearerHeader === undefined){
+    if (bearerHeader === '' || bearerHeader === undefined) {
         res.status(401).send('Unauthorized');
     }
     else {
         // Get the bearer
-    const bearer = bearerHeader!.replace(/^Bearer\s/, '');
-    if(bearer === '' || bearer === undefined){
-        res.status(401).send('Unauthorized');
-    }
-    // Decode the token
-    const jwtOutput = jwt.decode(bearer);
-    if(jwtOutput == null){
-        res.send(401).send('Invalid token');
-    }
-    const user_id = jwtOutput?.sub;
-    req.body.user_id  = user_id; // Add the user ID to the body
-    next();
+        const bearer = bearerHeader!.replace(/^Bearer\s/, '');
+        if (bearer === '' || bearer === undefined) {
+            res.status(401).send('Unauthorized');
+        }
+        // Decode the token
+        const jwtOutput = jwt.decode(bearer);
+        if (jwtOutput == null) {
+            res.send(401).send('Invalid token');
+        }
+        const user_id = jwtOutput?.sub;
+
+        const meta = JSON.parse(req.body['meta']);
+        const orgId = meta['tdei_org_id'];
+        // Also check for the authorization of the user 
+        // Needs to be authorized with tdeiOrgId, and should have any 
+        // of the `flex_data_generator`, `poc`, `tdei_admin` 
+        // this is to be done against the userID
+        const authProvider = Core.getAuthorizer({ provider: "Hosted", apiUrl: environment.authPermissionUrl });
+        const permissionRequest = new PermissionRequest({
+            userId: user_id as string,
+            orgId: orgId,
+            permssions: approvedRoles,
+            shouldSatisfyAll: false
+        });
+
+        try {
+            const response = await authProvider?.hasPermission(permissionRequest);
+            if (response) {
+                req.body.user_id = user_id;
+                next();
+            }
+            else {
+                res.send(401).send('Not Authorized');
+            }
+        }
+        catch (error) {
+            res.send(401).send('Not Authorized');
+        }
+
+
+
     }
 }
